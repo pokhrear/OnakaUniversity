91ddfbab2c2f9bc9fb6f84ddff9f8cc951fa7d91
Arjun <arjun_pokharel2000@yahoo.com>
2020-03-25 21:42:11 -0400

    Add project files.

---
+++ OnakaUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnakaUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ OnakaUniversity/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnakaUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [Display(Name ="Last Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + "" + FirstName;
+            }
+        }
+        public ICollection<Enrollment> Enrollments { get; set; }
+       
+
+    }
+}
